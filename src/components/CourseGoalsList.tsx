0fbfd255f54577601fe706e0d3d2b432839f30b7
Mauricio Conto <Mauricio.Conto@corecard.bog>
2026-01-06 11:48:16 -0500

    finish the project goals

--- src/components/CourseGoalsList.tsx
+++ src/components/CourseGoalsList.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from "react";
 import type { CourseGoal as CGoal} from "../App";
 import CourseGoals from "./CourseGoals";
+import InfoBox from "./InfoBox";
 
 type CourseGoalsProps = {
     goals: CGoal[];
@@ -8,9 +10,22 @@ type CourseGoalsProps = {
 
 export default function CourseGoalsList({goals, onDeleteGoal}: CourseGoalsProps){
 
+if (goals.length === 0){
+  return (
+  <InfoBox mode = "hint">You have no course goals yet. Start adding some!</InfoBox>
+);
+}
+
+let warningBox: ReactNode;
+
+if (goals.length >=4){
+  warningBox = (<InfoBox mode="warning" severity="medium"> You're collecting a lot of goals. Don't put too much on your plate!</InfoBox>);
+}
+
 return (
-<div>
-<ul className="goals-grid">
+  <>
+  {warningBox}
+  <ul>
     {goals.map(goal => (
       <li key={goal.id}>
         <CourseGoals title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
@@ -19,6 +34,6 @@ return (
       </li>
     ))}
   </ul>
-  </div>
+  </>
 );
 }
